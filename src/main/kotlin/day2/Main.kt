a931f55a995efc7e041e5dd14e7f2afbaf9d7748
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-05 05:12:25 -0500

    day2-2

--- src/main/kotlin/day2/Main.kt
+++ src/main/kotlin/day2/Main.kt
@@ -2,21 +2,63 @@ package day2
 
 import java.io.File
 
-fun main(args: Array<String>) {
-  val commands = File(args.first()).readLines()
+abstract class Processor {
+  abstract fun forward(i: Int)
+  abstract fun up(i: Int)
+  abstract fun down(i: Int)
 
-  val re = Regex("""\A(forward|up|down) (\d+)\z""")
   var depth = 0
   var x = 0
-  commands.forEach { cmd ->
-    val match = re.matchEntire(cmd)!!
-    val (name, distStr) = match.destructured
-    val dist = Integer.parseInt(distStr)
-    when (name) {
-      "forward" -> x += dist
-      "up" -> depth -= dist
-      "down" -> depth += dist
+  fun result() = depth * x
+
+  fun runLines(lines: List<String>): Processor {
+    val re = Regex("""\A(forward|up|down) (\d+)\z""")
+    lines.forEach { line ->
+      val (cmd, istr) = re.matchEntire(line)!!.destructured
+      val i = Integer.parseInt(istr)
+      when (cmd) {
+        "forward" -> forward(i)
+        "up" -> up(i)
+        "down" -> down(i)
+      }
     }
+    return this
   }
-  println(depth * x)
+}
+
+class Part1Processor : Processor() {
+  override fun forward(i: Int) {
+    x += i
+  }
+
+  override fun up(i: Int) {
+    depth -= i
+  }
+
+  override fun down(i: Int) {
+    depth += i
+  }
+}
+
+class Part2Processor : Processor() {
+  var aim = 0
+
+  override fun forward(i: Int) {
+    x += i
+    depth += aim * i
+  }
+
+  override fun up(i: Int) {
+    aim -= i
+  }
+
+  override fun down(i: Int) {
+    aim += i
+  }
+}
+
+fun main(args: Array<String>) {
+  val lines = File(args.first()).readLines()
+  println(Part1Processor().runLines(lines).result())
+  println(Part2Processor().runLines(lines).result())
 }
