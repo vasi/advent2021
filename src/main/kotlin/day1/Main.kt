d11e5683448eb2f99e638f18dca421386dcf6257
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-05 04:50:40 -0500

    day1-2

--- src/main/kotlin/day1/Main.kt
+++ src/main/kotlin/day1/Main.kt
@@ -2,7 +2,12 @@ package day1
 
 import java.io.File
 
+fun countIncreases(list: List<Int>): Int {
+  return list.windowed(2).count { (a, b) -> b > a }
+}
+
 fun main(args: Array<String>) {
   val depths = File(args.first()).readLines().map { Integer.parseInt(it) }
-  println(depths.windowed(2).count { (a, b) -> b > a })
+  println(countIncreases(depths))
+  println(countIncreases(depths.windowed(3).map { it.sum() }))
 }
