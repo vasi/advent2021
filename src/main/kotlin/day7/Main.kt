36a742ede795866520286d1a4fde5e1a7a5fb18a
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-07 00:38:51 -0500

    day7-2

--- src/main/kotlin/day7/Main.kt
+++ src/main/kotlin/day7/Main.kt
@@ -2,6 +2,7 @@ package day7
 
 import java.io.File
 import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 fun median(list: List<Int>): Int {
   return list.sorted().get(list.size / 2)
@@ -13,9 +14,25 @@ fun part1(positions: List<Int>): Int {
   return cost
 }
 
+fun cost2(positions: List<Int>, pos: Int): Int {
+  return positions.map {
+    val dist = (it - pos).absoluteValue
+    dist * (dist + 1) / 2
+  }.sum()
+}
+
+fun part2(positions: List<Int>): Int {
+  // Feels like it should be mean, but maybe I have to check both sides of it? Anyhow this is fast enough
+  val min = positions.minOrNull()!!
+  val max = positions.maxOrNull()!!
+  val pos = (min..max).minByOrNull { cost2(positions, it) }!!
+  return cost2(positions, pos)
+}
+
 fun main(args: Array<String>) {
   val positions = File(args.first()).readLines().first()
     .split(",").map { Integer.parseInt(it) }
 
   println(part1(positions))
+  println(part2(positions))
 }
