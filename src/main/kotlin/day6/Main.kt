d67d7322347f642f722c85c0a712611a0280f123
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-07 00:19:42 -0500

    day6-2

--- src/main/kotlin/day6/Main.kt
+++ src/main/kotlin/day6/Main.kt
@@ -2,14 +2,20 @@ package day6
 
 import java.io.File
 
-fun nextGen(fish: List<Int>): List<Int> {
-  return fish.flatMap {
-    if (it == 0) listOf(6, 8)
-    else listOf(it - 1)
-  }
+typealias Fishes = Map<Int, Long>
+
+fun nextGen(fish: Fishes): Fishes {
+  return fish
+    .flatMap { (k, v) ->
+      if (k == 0)
+        listOf(6 to v, 8 to v)
+      else listOf(k - 1 to v)
+    }
+    .groupingBy { it.first }
+    .fold(0) { a, e -> a + e.second }
 }
 
-fun simulate(fish: List<Int>, generations: Int): List<Int> {
+fun simulate(fish: Fishes, generations: Int): Fishes {
   var fs = fish
   repeat(generations) {
     fs = nextGen(fs)
@@ -17,8 +23,18 @@ fun simulate(fish: List<Int>, generations: Int): List<Int> {
   return fs
 }
 
-fun main(args: Array<String>) {
-  val fish = File(args.first()).readLines().first().split(",")
+fun countFish(fish: Fishes): Long {
+  return fish.values.sum()
+}
+
+fun parseFish(file: String): Fishes {
+  val ages = File(file).readLines().first().split(",")
     .map { Integer.parseInt(it) }
-  println(simulate(fish, 80).size)
+  return ages.groupingBy { it }.eachCount().mapValues { (_, v) -> v.toLong() }
+}
+
+fun main(args: Array<String>) {
+  val fish = parseFish(args.first())
+  println(countFish(simulate(fish, 80)))
+  println(countFish(simulate(fish, 256)))
 }
