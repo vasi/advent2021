a7520c29f78c8c53b6c87f669248072fa10bd0bc
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-10 00:15:43 -0500

    day10-2

--- src/main/kotlin/day10/Main.kt
+++ src/main/kotlin/day10/Main.kt
@@ -2,7 +2,12 @@ package day10
 
 import java.io.File
 
-fun firstIllegal(line: String): Char? {
+data class ParseResults(
+  val firstIllegal: Char?,
+  val closingChars: List<Char>,
+)
+
+fun parse(line: String): ParseResults {
   val pairs = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
   val stack = mutableListOf<Char>()
   for (c in line) {
@@ -11,19 +16,37 @@ fun firstIllegal(line: String): Char? {
     } else if (!stack.isEmpty() && stack.last() == c) {
       stack.removeLast()
     } else {
-      return c
+      return ParseResults(c, stack.reversed())
     }
   }
-  return null
+  return ParseResults(null, stack.reversed())
 }
 
 fun part1(lines: List<String>): Int {
-    val firstIllegals = lines.mapNotNull { firstIllegal(it) }
+  val firstIllegals = lines.mapNotNull { parse(it).firstIllegal }
   val scores = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
   return firstIllegals.map { scores[it]!! }.sum()
+}
+
+fun scoreClosingChars(chars: List<Char>): Long {
+  val scores = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+  var score = 0L
+  for (c in chars) {
+    score = score * 5 + scores[c]!!
+  }
+  return score
+}
+
+fun part2(lines: List<String>): Long {
+  val scores = lines.map { parse(it) }
+    .filter { it.firstIllegal == null }
+    .map { scoreClosingChars(it.closingChars) }
+    .sorted()
+  return scores[scores.size / 2]
 }
 
 fun main(args: Array<String>) {
   val lines = File(args.first()).readLines()
   println(part1(lines))
+  println(part2(lines))
 }
