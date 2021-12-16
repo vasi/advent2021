029f77cd9563b3ce9ec9cebd3ae24ed61ac26d62
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-16 09:31:36 -0500

    day16-2

--- src/main/kotlin/day16/Main.kt
+++ src/main/kotlin/day16/Main.kt
@@ -1,11 +1,14 @@
 package day16
 
 import java.io.File
+import java.lang.RuntimeException
 
-open class Value {
+abstract class Value {
   open fun packets(): List<Packet> {
     return emptyList()
   }
+
+  abstract fun value(): Long
 }
 
 data class Literal(val literal: Long) : Value() {
@@ -21,11 +24,15 @@ data class Literal(val literal: Long) : Value() {
       return Literal(ret)
     }
   }
+
+  override fun value(): Long {
+    return literal
+  }
 }
 
-data class Operator(val packets: List<Packet>) : Value() {
+data class Operator(val op: Int, val packets: List<Packet>) : Value() {
   companion object {
-    fun parse(bs: BitStream): Operator {
+    fun parse(op: Int, bs: BitStream): Operator {
       val packets = mutableListOf<Packet>()
       val lengthType = bs.readBit()
       if (lengthType == 0) {
@@ -40,12 +47,38 @@ data class Operator(val packets: List<Packet>) : Value() {
           packets.add(Packet.parse(bs))
         }
       }
-      return Operator(packets)
+      return Operator(op, packets)
     }
   }
 
   override fun packets(): List<Packet> {
     return packets
+  }
+
+  private fun nth(i: Int): Long {
+    return packets[i].value.value()
+  }
+
+  private fun values(): List<Long> {
+    return packets.map { it.value.value() }
+  }
+
+  private fun cmp(p : (Long, Long) -> Boolean): Long {
+    if (p(nth(0), nth(1))) return 1
+    else return 0
+  }
+
+  override fun value(): Long {
+    return when (op) {
+      0 -> values().sum()
+      1 -> values().reduce { a, b -> a * b }
+      2 -> values().minOrNull()!!
+      3 -> values().maxOrNull()!!
+      5 -> cmp { a, b -> a > b }
+      6 -> cmp { a, b -> a < b }
+      7 -> cmp { a, b -> a == b }
+      else -> throw RuntimeException("op $op out of range")
+    }
   }
 }
 
@@ -55,11 +88,12 @@ data class Packet(val version: Int, val type: Int, val value: Value) {
       val version = bs.readInt(3)
       val type = bs.readInt(3)
       val value = if (type == 4) Literal.parse(bs)
-        else Operator.parse(bs)
+        else Operator.parse(type, bs)
       return Packet(version, type, value)
     }
   }
 }
+
 
 class BitStream(hex: String) {
   private val bytes = hex.map { it.digitToInt(16) }
@@ -88,7 +122,7 @@ class BitStream(hex: String) {
 }
 
 fun versionSum(packet: Packet): Long {
-  return packet.version + packet.value.packets().map { versionSum(it) }.sum()
+  return packet.version + packet.value.packets().sumOf { versionSum(it) }
 }
 
 fun main(args: Array<String>) {
@@ -100,4 +134,5 @@ fun main(args: Array<String>) {
   val bs = BitStream(contents)
   val packet = Packet.parse(bs)
   println(versionSum(packet))
+  println(packet.value.value())
 }
