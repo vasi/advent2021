b18390a7c59ad205bf0079d81bfcac5ba6967faf
Dave Vasilevsky <dave@vasilevsky.ca>
2021-12-17 00:42:52 -0500

    day17-2

--- src/main/kotlin/day17/Main.kt
+++ src/main/kotlin/day17/Main.kt
@@ -1,29 +1,57 @@
 package day17
 
-fun yseq(yvel: Int): Sequence<Int> {
-  return sequence {
-    var y = 0
-    var dy = yvel
-    while (true) {
-      y += dy
-      dy -= 1
-      yield(y)
-    }
+data class Target(val xmin: Int, val xmax: Int, val ymin: Int, val ymax: Int) {
+  fun contains(x: Int, y: Int): Boolean {
+    return (x >= xmin) && (x <= xmax) && (y >= ymin) && (y <= ymax)
   }
 }
 
-fun yintersects(dy: Int, ymin: Int, ymax: Int): Boolean {
-  return yseq(dy).takeWhile { it >= ymin }.any { it <= ymax }
+fun intersects(xvel: Int, yvel: Int, target: Target): Boolean {
+  var x = 0
+  var y = 0
+  var dx = xvel
+  var dy = yvel
+
+  while (true) {
+    x += dx
+    y += dy
+    dx = if (dx == 0) 0 else dx - 1
+    dy -= 1
+
+    if (target.contains(x, y))
+      return true
+
+    if (x > target.xmax)
+      return false
+    if (y < target.ymin && dy < 0)
+      return false
+  }
 }
 
-fun ymax(dy: Int): Int {
-  return yseq(dy).zipWithNext().takeWhile { (a, b) -> a <= b }.last().second
+fun allIntersecting(target: Target): List<Pair<Int, Int>> {
+  val ret = mutableListOf<Pair<Int, Int>>()
+  for (xvel in 0..target.xmax) {
+    for (yvel in target.ymin .. -target.ymin) {
+      if (intersects(xvel, yvel, target)) {
+        ret.add(Pair(xvel, yvel))
+      }
+    }
+  }
+  return ret
 }
 
-fun part1(ymin: Int, ymax: Int): Int {
-  return (1..1000).filter { yintersects(it, ymin, ymax) }.maxOf { ymax(it) }
+fun part1(target: Target): Int {
+  return allIntersecting(target).map { it.second }.toSet().maxOf { dy -> dy * (dy+1) / 2 }
+}
+
+fun part2(target: Target): Int {
+  return allIntersecting(target).size
 }
 
 fun main(args: Array<String>) {
-  println(part1(-110, -69))
+  val target = Target(156, 202, -110, -69)
+//  val target = Target(20, 30, -10, -5)
+
+  println(part1(target))
+  println(part2(target))
 }
